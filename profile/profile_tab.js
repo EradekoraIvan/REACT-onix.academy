9c278b96443a95d312063ab84df21152fe2baa15
Novikov Ivan <eradekora@gmail.com>
2022-04-09 21:06:30 +0300

    js

---
+++ profile/profile_tab.js
@@ -0,0 +1,21 @@
+const tabs = document.getElementById("tabs"),
+      content = document.querySelectorAll(".content");
+
+
+    const chengeClass = el => {
+        for(let i = 0; i < tabs.children.length; i++) {
+            tabs.children[i].classList.remove('active');
+        }
+        el.classList.add('active');
+}
+
+tabs.addEventListener('click', e => {
+    const currTab = e.target.dataset.btn;
+    chengeClass(e.target);
+    for(let i=0; i < content.length; i++) {
+        content[i].classList.remove('active');
+        if(content[i].dataset.content === currTab) {
+            content[i].classList.add('active');
+        }
+    }
+})
