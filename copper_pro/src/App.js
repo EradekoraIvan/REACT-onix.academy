5ec4513060384a87e06f739c93bbeb56229e5214
Novikov Ivan <eradekora@gmail.com>
2022-05-06 01:22:52 +0300

    +react

--- copper_pro/src/App.js
+++ copper_pro/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Description from './component/landing_page/description/description.js';
+import FirstScreen from './component/landing_page/feirst_screen/first_screen.js';
+import Main from './component/landing_page/main/main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Description/>
+        <FirstScreen/>
+        <Main/>
     </div>
   );
 }
