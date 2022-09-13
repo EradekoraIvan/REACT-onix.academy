ddd9144fa3e3827fe71e23a8f944c1931e857acf
Novikov Ivan <eradekora@gmail.com>
2022-09-13 15:02:04 +0300

    dell description

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../component/header/header';
 import Footer from '../component/footer/footer';
-import Description from '../pages/landing_page/description/description';
+// Description from '../pages/landing_page/description/description';
 import style from './Layout.module.css';
 
 export default function Layout() {
   return (
     <>
-      <Description />
+      
       <Header headerStyle="header_style" />
       <main className={style.main}><Outlet /></main>
       <Footer />
--- src/pages/landing_page/landingPage.js
+++ src/pages/landing_page/landingPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Description from './description/description';
+//import Description from './description/description';
 import FirstScreen from './feirst_screen/first_screen';
 import Main from './main/main';
 
 function LandingPage() {
   return (
     <div>
-      <Description />
+    
       <FirstScreen />
       <Main />
     </div>
